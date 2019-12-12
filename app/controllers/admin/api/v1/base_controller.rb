74bbbb0da36cf157a2c7e4238823d3feafc3dc95
Laura Schulz <lrs8810@gmail.com>
2019-12-12 08:08:10 -0700

    DRY code to BaseController
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

--- app/controllers/admin/api/v1/base_controller.rb
+++ app/controllers/admin/api/v1/base_controller.rb
@@ -1,19 +1,3 @@
-class Admin::Api::V1::BaseController < ActionController::API
+class Admin::Api::V1::BaseController < Admin::BaseController
   before_action :require_admin!
-
-  def require_admin!
-    four_oh_four unless current_user.admin?
-  end
-
-  def current_user
-    @current_user ||= if session[:user_id]
-                        User.find(session[:user_id])
-                      else
-                        User.new
-                      end
-  end
-
-  def four_oh_four
-    raise ActionController::RoutingError, 'Not Found'
-  end
 end
